fecb9b06f41a0abba72b0767da3cfd0c8bbdbcc4
Pablo Garrido <pablogs9@gmail.com>
2020-03-02 08:08:43 +0100

    Olimex FreeRTOS

---
+++ apps/olimex_sample_app/micro-ros-conf.sh
@@ -0,0 +1,6 @@
+update_meta "rmw_microxrcedds" "RMW_UXRCE_MAX_NODES=1"
+update_meta "rmw_microxrcedds" "RMW_UXRCE_MAX_PUBLISHERS=1"
+update_meta "rmw_microxrcedds" "RMW_UXRCE_MAX_SUBSCRIPTIONS=0"
+update_meta "rmw_microxrcedds" "RMW_UXRCE_MAX_SERVICES=1"
+update_meta "rmw_microxrcedds" "RMW_UXRCE_MAX_CLIENTS=0"
+update_meta "rmw_microxrcedds" "RMW_UXRCE_MAX_HISTORY=4"
