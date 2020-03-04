54e8064cfd625a3bc93293f00d811e14d8cdb044
Pablo Garrido <pablogs9@gmail.com>
2020-03-04 10:05:08 +0100

    Update CF21 procedure

--- apps/crazyflie_sample_app/micro-ros-conf.sh
+++
@@ -1,6 +0,0 @@
-update_meta "rmw_microxrcedds" "RMW_UXRCE_MAX_NODES=1"
-update_meta "rmw_microxrcedds" "RMW_UXRCE_MAX_PUBLISHERS=2"
-update_meta "rmw_microxrcedds" "RMW_UXRCE_MAX_SUBSCRIPTIONS=1"
-update_meta "rmw_microxrcedds" "RMW_UXRCE_MAX_SERVICES=1"
-update_meta "rmw_microxrcedds" "RMW_UXRCE_MAX_CLIENTS=0"
-update_meta "rmw_microxrcedds" "RMW_UXRCE_MAX_HISTORY=1"
