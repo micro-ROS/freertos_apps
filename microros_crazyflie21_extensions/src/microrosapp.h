642d2c86a833995613b1be2ce76826912914c4e3
Pablo Garrido <pablogs9@gmail.com>
2020-03-05 07:52:32 +0100

    Added demo malaga app

--- microros_crazyflie21_extensions/src/microrosapp.h
+++ microros_crazyflie21_extensions/src/microrosapp.h
@@ -12,7 +12,7 @@ static int usedMemory;
 inline static void * __crazyflie_allocate(size_t size, void * state)
 {
   (void) state;
-  DEBUG_PRINT("-- Alloc %d (prev: %d B)\n",size, xPortGetFreeHeapSize());
+  // DEBUG_PRINT("-- Alloc %d (prev: %d B)\n",size, xPortGetFreeHeapSize());
   absoluteUsedMemory += size;
   usedMemory += size;
   return pvPortMalloc(size);
@@ -21,7 +21,7 @@ inline static void * __crazyflie_allocate(size_t size, void * state)
 inline static void __crazyflie_deallocate(void * pointer, void * state)
 {
   (void) state;
-  DEBUG_PRINT("-- Free %d (prev: %d B)\n",getBlockSize(pointer), xPortGetFreeHeapSize());
+  // DEBUG_PRINT("-- Free %d (prev: %d B)\n",getBlockSize(pointer), xPortGetFreeHeapSize());
   usedMemory -= getBlockSize(pointer);
   vPortFree(pointer);
 }
@@ -29,7 +29,7 @@ inline static void __crazyflie_deallocate(void * pointer, void * state)
 inline static void * __crazyflie_reallocate(void * pointer, size_t size, void * state)
 {
   (void) state;
-  DEBUG_PRINT("-- Realloc %d -> %d (prev: %d B)\n",getBlockSize(pointer),size, xPortGetFreeHeapSize());
+  // DEBUG_PRINT("-- Realloc %d -> %d (prev: %d B)\n",getBlockSize(pointer),size, xPortGetFreeHeapSize());
   absoluteUsedMemory += size;
   usedMemory += size;
   usedMemory -= getBlockSize(pointer);
@@ -39,7 +39,7 @@ inline static void * __crazyflie_reallocate(void * pointer, size_t size, void * 
 inline static void * __crazyflie_zero_allocate(size_t number_of_elements, size_t size_of_element, void * state)
 {
   (void) state;
-  DEBUG_PRINT("-- Calloc %d x %d = %d -> (prev: %d B)\n",number_of_elements,size_of_element, number_of_elements*size_of_element, xPortGetFreeHeapSize());
+  // DEBUG_PRINT("-- Calloc %d x %d = %d -> (prev: %d B)\n",number_of_elements,size_of_element, number_of_elements*size_of_element, xPortGetFreeHeapSize());
   absoluteUsedMemory += number_of_elements*size_of_element;
   usedMemory += number_of_elements*size_of_element;
   return pvPortCalloc(number_of_elements,size_of_element);
