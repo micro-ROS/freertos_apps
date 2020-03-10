1e9f29b7d3cbb71ee6eafa4bcbe7a3e393a9c086
Pablo Garrido <pablogs9@gmail.com>
2020-03-10 12:29:32 +0100

    Refactor ping pong app

--- apps/olimex_ping_pong/app.c
+++ apps/olimex_ping_pong/app.c
@@ -12,6 +12,8 @@
 #include <unistd.h>
 #include <pthread.h>
 
+#define STRING_BUFFER_LEN 100
+
 // FreeRTOS thread for triggering a publication guard condition
 void * trigger_guard_condition(void *args){
   rcl_guard_condition_t * guard_condition = (rcl_guard_condition_t *)args;
@@ -31,8 +33,8 @@ void appMain(void *argument)
   rcl_init_options_init(&options, rcl_get_default_allocator());
 
   // Set Micro-XRCE-DDS client key
-  rmw_init_options_t* rmw_options = rcl_init_options_get_rmw_init_options(&options);
-  rmw_uros_options_set_client_key(0xBA5EBA11, rmw_options);
+  // rmw_init_options_t* rmw_options = rcl_init_options_get_rmw_init_options(&options);
+  // rmw_uros_options_set_client_key(0xBA5EBA11, rmw_options);
   
   // Init RCL context
   rcl_context_t context = rcl_get_zero_initialized_context();
@@ -46,22 +48,22 @@ void appMain(void *argument)
   // Create a ping ping_publisher
   rcl_publisher_options_t ping_publisher_ops = rcl_publisher_get_default_options();
   rcl_publisher_t ping_publisher = rcl_get_zero_initialized_publisher();
-  rcl_publisher_init(&ping_publisher, &node, ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Header), "/micro-ROS/ping", &ping_publisher_ops);
+  rcl_publisher_init(&ping_publisher, &node, ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Header), "/microROS/ping", &ping_publisher_ops);
 
   // Create a ping pong_publisher
   rcl_publisher_options_t pong_publisher_ops = rcl_publisher_get_default_options();
   rcl_publisher_t pong_publisher = rcl_get_zero_initialized_publisher();
-  rcl_publisher_init(&pong_publisher, &node, ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Header), "/micro-ROS/pong", &pong_publisher_ops);
+  rcl_publisher_init(&pong_publisher, &node, ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Header), "/microROS/pong", &pong_publisher_ops);
 
   // Create a pong subscriber
   rcl_subscription_options_t pong_subscription_ops = rcl_subscription_get_default_options();
   rcl_subscription_t pong_subscription = rcl_get_zero_initialized_subscription();
-  rcl_subscription_init(&pong_subscription, &node, ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Header), "/micro-ROS/pong", &pong_subscription_ops);
+  rcl_subscription_init(&pong_subscription, &node, ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Header), "/microROS/pong", &pong_subscription_ops);
 
   // Create a ping subscriber
   rcl_subscription_options_t ping_subscription_ops = rcl_subscription_get_default_options();
   rcl_subscription_t ping_subscription = rcl_get_zero_initialized_subscription();
-  rcl_subscription_init(&ping_subscription, &node, ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Header), "/micro-ROS/ping", &ping_subscription_ops);
+  rcl_subscription_init(&ping_subscription, &node, ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Header), "/microROS/ping", &ping_subscription_ops);
 
   // Create a guard condition
   rcl_guard_condition_t guard_condition = rcl_get_zero_initialized_guard_condition();
@@ -74,15 +76,22 @@ void appMain(void *argument)
 
   // Create a wait set
   rcl_wait_set_t wait_set = rcl_get_zero_initialized_wait_set();
-  rcl_wait_set_init(&wait_set, 2, 1, 0, 0, 1, 0, &context, rcl_get_default_allocator());
+  rcl_wait_set_init(&wait_set, 2, 1, 0, 0, 0, 0, &context, rcl_get_default_allocator());
 
   // Create and allocate the pingpong message
   std_msgs__msg__Header msg;
   
-  char msg_buffer[100];
+  char msg_buffer[STRING_BUFFER_LEN];
   msg.frame_id.data = msg_buffer;
-  msg.frame_id.capacity = 100;
+  msg.frame_id.capacity = STRING_BUFFER_LEN;
 
+  std_msgs__msg__Header rcv_msg;
+
+  char rcv_buffer[STRING_BUFFER_LEN];
+  rcv_msg.frame_id.data = rcv_buffer;
+  rcv_msg.frame_id.capacity = STRING_BUFFER_LEN;
+
+  int device_id = rand();
   int seq_no = rand();
   sprintf(msg.frame_id.data, "%d", seq_no);
   msg.frame_id.size = strlen(msg.frame_id.data);
@@ -90,7 +99,9 @@ void appMain(void *argument)
   // frame_id should contain node identity
   
   int pong_count = 0;
+  struct timespec ts;
   rcl_ret_t rc;
+
   do {
     // Clear and set the waitset
     rcl_wait_set_clear(&wait_set);
@@ -109,9 +120,15 @@ void appMain(void *argument)
 
     // Check if it is time to send a ping
     if (wait_set.guard_conditions[index_guardcondition]) {
-      int seq_no = rand();
-      sprintf(msg.frame_id.data, "%d", seq_no);
+      seq_no = rand();
+      sprintf(msg.frame_id.data, "%d_%d", seq_no, device_id);
       msg.frame_id.size = strlen(msg.frame_id.data);
+      
+      clock_gettime(CLOCK_REALTIME, &ts);
+
+      msg.stamp.sec = ts.tv_sec;
+      msg.stamp.nanosec = ts.tv_nsec;
+
       pong_count = 0;
       rcl_publish(&ping_publisher, (const void*)&msg, NULL);
       // printf("Ping send seq 0x%x\n", seq_no);
@@ -119,11 +136,9 @@ void appMain(void *argument)
     
     // Check if some pong is received
     if (wait_set.subscriptions[index_pong_subscription]) {
-      std_msgs__msg__Header rcv_msg;
       rc = rcl_take(wait_set.subscriptions[index_pong_subscription], &rcv_msg, NULL, NULL);
-      int recv_seq_no = atoi(rcv_msg.frame_id.data);
 
-      if(rc == RCL_RET_OK && recv_seq_no == seq_no) {
+      if(rc == RCL_RET_OK && strcmp(msg.frame_id.data,rcv_msg.frame_id.data) == 0) {
           pong_count++;
           // printf("Pong for seq 0x%x (%d)\n", seq_no, pong_count);
       }
@@ -131,12 +146,10 @@ void appMain(void *argument)
 
     // Check if some ping is received and answer it
     if (wait_set.subscriptions[index_ping_subscription]) {
-      std_msgs__msg__Header rcv_msg;
       rc = rcl_take(wait_set.subscriptions[index_ping_subscription], &rcv_msg, NULL, NULL);
-      int recv_seq_no = atoi(rcv_msg.frame_id.data);
 
       // Dont pong my own pings
-      if(rc == RCL_RET_OK && recv_seq_no != seq_no){
+      if(rc == RCL_RET_OK && strcmp(msg.frame_id.data,rcv_msg.frame_id.data) != 0){
         // printf("Ping received with seq 0x%x (%d). Answering.\n", seq_no);
         rcl_publish(&pong_publisher, (const void*)&rcv_msg, NULL);
       }
