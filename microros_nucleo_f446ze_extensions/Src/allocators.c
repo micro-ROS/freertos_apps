935b74de5ee7388257aea68913aac57a076179a7
alsaibie <alsaibie@gmail.com>
2020-06-26 22:36:42 +0300

    allocate instead of reallocate if ptr is null, temporary fix?

--- microros_nucleo_f446ze_extensions/Src/allocators.c
+++ microros_nucleo_f446ze_extensions/Src/allocators.c
@@ -13,22 +13,27 @@ void * __freertos_allocate(size_t size, void * state){
 
 void __freertos_deallocate(void * pointer, void * state){
   (void) state;
-  if( NULL != pointer){
+ if( NULL != pointer){
     // printf("-- Free %d (prev: %d B)\n",getBlockSize(pointer), xPortGetFreeHeapSize());
     usedMemory -= getBlockSize(pointer);
     vPortFree(pointer);
-  }
+ }
 }
 
 void * __freertos_reallocate(void * pointer, size_t size, void * state){
   (void) state;
   // printf("-- Realloc %d -> %d (prev: %d B)\n",getBlockSize(pointer),size, xPortGetFreeHeapSize());
-  // if (NULL != pointer){
+  if (NULL != pointer){
     absoluteUsedMemory += size;
     usedMemory += size;
     usedMemory -= getBlockSize(pointer);
     return pvPortRealloc(pointer,size);
-  // }
+  }
+  else {
+      absoluteUsedMemory += size;
+      usedMemory += size;
+      return pvPortMalloc(size);
+  }
 }
 
 void * __freertos_zero_allocate(size_t number_of_elements, size_t size_of_element, void * state){
