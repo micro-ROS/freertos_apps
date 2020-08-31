0092c011cf411b129969a9f978c8ccb490bbf7ca
francesca <francescafinocchiaro@eprosima.com>
2020-08-31 07:52:49 +0200

    New script for memo profiling graphs.

---
+++ apps/xrce_dds_publisher_profiler/overall_graph.py
@@ -0,0 +1,73 @@
+#import msparser
+import os
+import signal
+import subprocess
+import sys
+import time
+from tabulate import tabulate
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+from matplotlib.ticker import EngFormatter
+
+names = ["results-XML-RELIABLE.txt", "results-XML-BEST_EFFORT.txt", "results-REF-RELIABLE.txt", "results-REF-BEST_EFFORT.txt"]
+
+descriptions = ["XML & Reliable streams",
+                "XML & Best-effort streams",
+                "References & Reliable streams",
+                "References & Best-effort streams"]
+
+total_usages_steady = []
+
+for name, desc in zip(names, descriptions):
+    with open(name,'r') as p:
+        file = p.read()
+        
+    file = " ".join(file.split("\n")[1:])
+    data = [int(x) for x in file.split(" ") if len(x)]
+    data = np.reshape(data, (40, 4))
+    
+    topic_sizes = list(dict.fromkeys([x[1] for x in data]))
+    entities_sizes = list(dict.fromkeys([x[0] for x in data]))
+    
+    usages_steady = []
+
+    for entities_size in entities_sizes:
+        for topic_size in topic_sizes:
+            line = [x for x in data if x[0] == entities_size and x[1] == topic_size][0]
+
+            min_buff = 400 if "XML" in name else 150
+            topic_size_adjs = min_buff if topic_size + 32 < min_buff else topic_size + 32
+
+            usages_steady.append(line[2]+line[3]-topic_size_adjs)  #Removing topic size buffer since topic data memory can be shared with data source
+            
+    usages_steady = np.reshape(usages_steady, (len(entities_sizes), len(topic_sizes)))
+    total_usages_steady.append(usages_steady)
+
+fig, axs = plt.subplots(2, 2, sharex='col', sharey='row')
+axs = axs.flatten()
+
+formatter1 = EngFormatter(places=1, sep="\N{THIN SPACE}")
+
+for i in range(len(names)):
+    for j,n in zip(total_usages_steady[i], entities_sizes):
+        axs[i].plot(topic_sizes, j, 'o-', markersize=1.5, linewidth=1, label="{:d} pubs".format(n))
+        axs[i].legend(frameon=False, fontsize='small')
+        axs[i].set_ylim([2000,8000])
+        axs[i].set_title(descriptions[i], fontsize=10)
+        axs[i].grid(b=True, which='major', linestyle='--', color='grey', linewidth=0.3)
+        axs[i].grid(b=True, which='minor', linestyle='--', color='grey', linewidth=0.3)
+        axs[i].xaxis.set_major_formatter(formatter1)
+        axs[i].yaxis.set_major_formatter(formatter1)
+        axs[i].tick_params(axis='both', which='major', labelsize=7)
+
+fig.text(0.5, 1.02, 'Publisher application', ha='center', fontsize=15)
+
+fig.text(0.5, -0.03, 'Topic size (B)', ha='center', fontsize=10)
+fig.text(-0.04, 0.5, 'Memory usage (B)', va='center', rotation='vertical', fontsize=11)
+
+fig.tight_layout()
+
+plt.savefig("overall_pub" + ".svg", dpi=600, bbox_inches='tight')
+plt.savefig("overall_pub" + ".png", dpi=600, bbox_inches='tight')
