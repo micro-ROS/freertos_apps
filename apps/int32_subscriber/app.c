26e5085d9ac7b3da93745e96132420a622aa9448
Pablo Garrido <pablogs9@gmail.com>
2020-09-02 12:12:52 +0200

    subscriber

--- apps/int32_subscriber/app.c
+++ apps/int32_subscriber/app.c
@@ -5,7 +5,18 @@
 
 #include <std_msgs/msg/int32.h>
 
+#ifdef ESP_PLATFORM
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#endif
+
 #include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include "esp_timer.h"
+#include "esp_log.h"
+#include "esp_sleep.h"
+#include "sdkconfig.h"
 
 #define RCCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){printf("Failed status on line %d: %d. Aborting.\n",__LINE__,(int)temp_rc);vTaskDelete(NULL);}}
 #define RCSOFTCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){printf("Failed status on line %d: %d. Continuing.\n",__LINE__,(int)temp_rc);}}
@@ -13,14 +24,69 @@
 rcl_subscription_t subscriber;
 std_msgs__msg__Int32 msg;
 
+int absoluteUsedMemory = 0;
+int usedMemory = 0;
+
+uint8_t count = 0;
+
 void subscription_callback(const void * msgin)
 {
 	const std_msgs__msg__Int32 * msg = (const std_msgs__msg__Int32 *)msgin;
 	printf("Received: %d\n", msg->data);
+	count++;
+}
+
+void * __freertos_allocate(size_t size, void * state){
+  (void) state;
+  // printf("-- Alloc %d (prev: %d B)\n",size, xPortGetFreeHeapSize());
+  absoluteUsedMemory += size;
+  usedMemory += size;
+  return malloc(size);
+}
+
+void __freertos_deallocate(void * pointer, void * state){
+  (void) state;
+  // printf("-- Free %d (prev: %d B)\n",getBlockSize(pointer), xPortGetFreeHeapSize());
+  if (NULL != pointer){
+    // usedMemory -= getBlockSize(pointer);
+    free(pointer);
+  }
+}
+
+void * __freertos_reallocate(void * pointer, size_t size, void * state){
+  (void) state;
+  // printf("-- Realloc %d -> %d (prev: %d B)\n",getBlockSize(pointer),size, xPortGetFreeHeapSize());
+  absoluteUsedMemory += size;
+  usedMemory += size;
+  if (NULL == pointer){
+    return malloc(size);
+  } else {
+    // usedMemory -= getBlockSize(pointer);
+    return realloc(pointer,size);
+  }
+}
+
+void * __freertos_zero_allocate(size_t number_of_elements, size_t size_of_element, void * state){
+  (void) state;
+  // printf("-- Calloc %d x %d = %d -> (prev: %d B)\n",number_of_elements,size_of_element, number_of_elements*size_of_element, xPortGetFreeHeapSize());
+  absoluteUsedMemory += number_of_elements*size_of_element;
+  usedMemory += number_of_elements*size_of_element;
+  return calloc(number_of_elements,size_of_element);
 }
 
 void appMain(void * arg)
 {
+
+    rcl_allocator_t freeRTOS_allocator = rcutils_get_zero_initialized_allocator();
+    freeRTOS_allocator.allocate = __freertos_allocate;
+    freeRTOS_allocator.deallocate = __freertos_deallocate;
+    freeRTOS_allocator.reallocate = __freertos_reallocate;
+    freeRTOS_allocator.zero_allocate = __freertos_zero_allocate;
+
+    if (!rcutils_set_default_allocator(&freeRTOS_allocator)) {
+        printf("Error on default allocators (line %d)\n",__LINE__); 
+    }
+
   	rcl_allocator_t allocator = rcl_get_default_allocator();
 	rclc_support_t support;
 
@@ -46,11 +112,17 @@ void appMain(void * arg)
 	RCCHECK(rclc_executor_set_timeout(&executor, RCL_MS_TO_NS(rcl_wait_timeout)));
 	RCCHECK(rclc_executor_add_subscription(&executor, &subscriber, &msg, &subscription_callback, ON_NEW_DATA));
 
-        while(1){
-                rclc_executor_spin_some(&executor, 100);
-                usleep(100000);
-        }
+	while(count<10){
+			rclc_executor_spin_some(&executor, 100);
+			usleep(100000);
+	}
 
+    printf("Dynamic absolute: %d\n",absoluteUsedMemory);
+    printf("Dynamic total: %d\n",usedMemory);
+
+    UBaseType_t uxHighWaterMark;
+    uxHighWaterMark = uxTaskGetStackHighWaterMark( NULL );
+	printf("Stack peak: %d\n", (12*2048*4) - uxHighWaterMark*4);
 
 	RCCHECK(rcl_subscription_fini(&subscriber, &node));
 	RCCHECK(rcl_node_fini(&node));
