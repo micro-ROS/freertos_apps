102fd04273cc7a924b010a62244a8ec317de9377
Eden Desta <edesta@boston-engineering.com>
2020-09-28 13:20:14 -0400

    Adding the nucleo f746zg extensions to freertos apps

---
+++ microros_nucleo_f746zg_extensions/Src/allocators.c
@@ -0,0 +1,42 @@
+#include <allocators.h>
+
+int absoluteUsedMemory = 0;
+int usedMemory = 0;
+
+void * __freertos_allocate(size_t size, void * state){
+  (void) state;
+  // printf("-- Alloc %d (prev: %d B)\n",size, xPortGetFreeHeapSize());
+  absoluteUsedMemory += size;
+  usedMemory += size;
+  return pvPortMalloc(size);
+}
+
+void __freertos_deallocate(void * pointer, void * state){
+  (void) state;
+  // printf("-- Free %d (prev: %d B)\n",getBlockSize(pointer), xPortGetFreeHeapSize());
+  if (NULL != pointer){
+    usedMemory -= getBlockSize(pointer);
+    vPortFree(pointer);
+  }
+}
+
+void * __freertos_reallocate(void * pointer, size_t size, void * state){
+  (void) state;
+  // printf("-- Realloc %d -> %d (prev: %d B)\n",getBlockSize(pointer),size, xPortGetFreeHeapSize());
+  absoluteUsedMemory += size;
+  usedMemory += size;
+  if (NULL == pointer){
+    return pvPortMalloc(size);
+  } else {
+    usedMemory -= getBlockSize(pointer);
+    return pvPortRealloc(pointer,size);
+  }
+}
+
+void * __freertos_zero_allocate(size_t number_of_elements, size_t size_of_element, void * state){
+  (void) state;
+  // printf("-- Calloc %d x %d = %d -> (prev: %d B)\n",number_of_elements,size_of_element, number_of_elements*size_of_element, xPortGetFreeHeapSize());
+  absoluteUsedMemory += number_of_elements*size_of_element;
+  usedMemory += number_of_elements*size_of_element;
+  return pvPortCalloc(number_of_elements,size_of_element);
+}
