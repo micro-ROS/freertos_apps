0c340fc5019ea8550a338605b6df7e1c404e3e47
DorBenHarush <dor2981123@gmail.com>
2020-10-28 22:39:24 +0200

    compressed image instead of raw

--- apps/take_picture/app.c
+++ apps/take_picture/app.c
@@ -20,7 +20,8 @@
 
 #include <rcl/rcl.h>
 #include <rcl/error_handling.h>
-#include <sensor_msgs/msg/image.h>
+#include <sensor_msgs/msg/compressed_image.h>
+
 
 #include <rclc/rclc.h>
 #include <rclc/executor.h>
@@ -29,15 +30,16 @@
 #include "freertos/task.h"
 #include "boards.h"
 
-#define STRING_CAPACITY 20
 
 static const char *TAG = "example:take_picture";
+
+
 
 #define RCCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){printf("Failed status on line %d: %d. Aborting.\n",__LINE__,(int)temp_rc);vTaskDelete(NULL);}}
 #define RCSOFTCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){printf("Failed status on line %d: %d. Continuing.\n",__LINE__,(int)temp_rc);}}
 
 rcl_publisher_t publisher;
-sensor_msgs__msg__Image* msg;
+sensor_msgs__msg__CompressedImage* msg;
 camera_fb_t *pic;
 
 
@@ -65,8 +67,8 @@ static camera_config_t camera_config = {
     .ledc_timer = LEDC_TIMER_0,
     .ledc_channel = LEDC_CHANNEL_0,
 
-    .pixel_format = PIXFORMAT_YUV422, //YUV422,GRAYSCALE,RGB565,JPEG
-    .frame_size = FRAMESIZE_QQVGA,    //QQVGA-UXGA Do not use sizes above QVGA when not JPEG
+    .pixel_format = PIXFORMAT_JPEG, //YUV422,GRAYSCALE,RGB565,JPEG
+    .frame_size = FRAMESIZE_HVGA,    //QQVGA-UXGA Do not use sizes above QVGA when not JPEG
 
     .jpeg_quality = 12, //0-63 lower number means higher quality
     .fb_count = 1       //if more than one, i2s runs in continuous mode. Use only with JPEG
@@ -95,10 +97,10 @@ static esp_err_t init_camera()
 
 void appMain(void * arg)
 {
-    init_camera();
-    rcl_allocator_t allocator = rcl_get_default_allocator();
+	init_camera();
+	rcl_allocator_t allocator = rcl_get_default_allocator();
 	rclc_support_t support;
-	msg = sensor_msgs__msg__Image__create();
+	msg = sensor_msgs__msg__CompressedImage__create();
 	// create init_options
 	RCCHECK(rclc_support_init(&support, 0, NULL, &allocator));
 
@@ -110,7 +112,7 @@ void appMain(void * arg)
 	RCCHECK(rclc_publisher_init_default(
 		&publisher,
 		&node,
-		ROSIDL_GET_MSG_TYPE_SUPPORT(sensor_msgs, msg, Image),
+		ROSIDL_GET_MSG_TYPE_SUPPORT(sensor_msgs, msg, CompressedImage),
 		"freertos_picture_publisher"));
 
 	// create timer,
@@ -130,69 +132,54 @@ void appMain(void * arg)
 	RCCHECK(rclc_executor_set_timeout(&executor, RCL_MS_TO_NS(rcl_wait_timeout)));
 	RCCHECK(rclc_executor_add_timer(&executor, &timer));
 
-	//takes picture in yuv422 format
-	pic = esp_camera_fb_get();	
-	
-	//set height, width, step and is_bigedian
-	msg->height = pic->height;
-	msg->width = pic->width;
-	msg->is_bigendian = 0;
-	msg->step = pic->width * 2;
-
-	//set pixel format and frame id
-	char * encoding = "yuv422";
-	msg->encoding.data = (char*)heap_caps_malloc(strlen(encoding) + 1, MALLOC_CAP_8BIT); 
-	msg->header.frame_id.capacity = STRING_CAPACITY;
-	memset(msg->encoding.data, 0, strlen(encoding) + 1);
-	memcpy(msg->encoding.data, encoding, strlen(encoding));
-	msg->encoding.size = strlen(encoding) + 1;
+	char * encoding = "jpeg";
+	msg->format.data = (char*)heap_caps_malloc(strlen(encoding) + 1, MALLOC_CAP_8BIT); 
+	msg->format.capacity = strlen(encoding) + 1;
+	memset(msg->format.data, 0, strlen(encoding) + 1);
+	memcpy(msg->format.data, encoding, strlen(encoding));
+	msg->format.size = strlen(encoding) + 1;
 
 	char* frame_id = "AI_thinker_image";
 	msg->header.frame_id.data = (char*)heap_caps_malloc(strlen(frame_id) + 1, MALLOC_CAP_8BIT);
-	msg->header.frame_id.capacity = STRING_CAPACITY;
+	msg->header.frame_id.capacity = strlen(frame_id) + 1;
 	memset(msg->header.frame_id.data, 0, strlen(frame_id) + 1);
 	memcpy(msg->header.frame_id.data, frame_id, strlen(frame_id));
 	msg->header.frame_id.size = strlen(frame_id) + 1;
-
-
-	//allocate memory for data
-	msg->data.data = (uint8_t*)heap_caps_malloc(pic->len, MALLOC_CAP_8BIT);
-	msg->data.capacity = pic->len;
-	msg->data.size = msg->step * pic->height ;
-	printf("copying data...\n");
-	if(msg->data.data == NULL){
-		printf("failed to allocate memory. restating in 5 sec.\n");
-		vTaskDelay(5000 / portTICK_RATE_MS);
-		esp_restart();
-	}	
-	memcpy(msg->data.data, pic->buf, pic->len);
-
-	//msg->header.stamp ?
-
-	printf("height: %d \n", msg->height);
-	printf("width: %d \n", msg->width);
-	printf("pixel format %s \n", msg->encoding.data);
-	printf("is bigendian %d \n", msg->is_bigendian);
-	printf("picture step %d \n", msg->step);
-	printf("frame id %s \n", msg->header.frame_id.data);
-	printf("buffer length %d \n", msg->data.size);
-	printf("pic->len %d \n", pic->len);
-
 	
-	while(1){
+
+	while(1)
+	{
 		rclc_executor_spin_some(&executor, 1000);
 		usleep(100000);
+		pic = esp_camera_fb_get();
+		//allocate memory for data
+        	msg->data.data = (uint8_t*)heap_caps_malloc(pic->len, MALLOC_CAP_8BIT);
+        	msg->data.capacity = pic->len;
+        	msg->data.size = pic->len;
+        	printf("coopying data...\n");
+        	if(msg->data.data == NULL)
+		{
+			printf("failed to allocate memory. restating in 5 sec.\n");
+			vTaskDelay(5000 / portTICK_RATE_MS);
+			esp_restart();
+		}
+		memcpy(msg->data.data, pic->buf, pic->len);
+       
+		printf("pixel format %s \n", msg->format.data);
+		printf("frame id %s \n", msg->header.frame_id.data);
+		printf("buffer length %d \n", msg->data.size);
+
 		//publish picture
 		printf("publisher return value: %d \n", rcl_publish(&publisher, msg, NULL));
-       	vTaskDelay(1000 / portTICK_RATE_MS);
+		vTaskDelay(200 / portTICK_RATE_MS);
 	}
 
-    // free resources
+    	// free resources
 	RCCHECK(rcl_publisher_fini(&publisher, &node))
 	RCCHECK(rcl_node_fini(&node))
-	heap_caps_free(msg->encoding.data);
+	heap_caps_free(msg->format.data);
 	heap_caps_free(msg->header.frame_id.data);
 	heap_caps_free(msg->data.data);
-	sensor_msgs__msg__Image__destroy(msg);
+	sensor_msgs__msg__CompressedImage__destroy(msg);
 	vTaskDelete(NULL);
 }
