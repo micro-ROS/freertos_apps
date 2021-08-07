b2fc1ef1fc76ce5e5b601e6660ab55f95c5092d4
Linda TJ <sameer.tuteja05@gmail.com>
2021-08-07 17:37:16 +0530

    Create app.c

---
+++ apps/string_publisher/app.c
@@ -0,0 +1,82 @@
+#include <stdio.h>
+#include <unistd.h>
+
+#include <rcl/rcl.h>
+#include <rcl/error_handling.h>
+#include <std_msgs/msg/string.h>
+
+#include <rclc/rclc.h>
+#include <rclc/executor.h>
+
+#ifdef ESP_PLATFORM
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#endif
+
+#define RCCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){printf("Failed status on line %d: %d. Aborting.\n",__LINE__,(int)temp_rc);vTaskDelete(NULL);}}
+#define RCSOFTCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){printf("Failed status on line %d: %d. Continuing.\n",__LINE__,(int)temp_rc);}}
+
+//initialization
+rcl_publisher_t publisher;
+std_msgs__msg__String msg;
+int counter = 1;
+
+void timer_callback(rcl_timer_t * timer, int64_t last_call_time)
+{
+	RCLC_UNUSED(last_call_time);
+	if (timer != NULL) {
+		// assign message to publisher
+  		std_msgs__msg__String__init(&msg);
+  		const unsigned int PUB_MSG_CAPACITY = 30;
+  		msg.data.data = malloc(PUB_MSG_CAPACITY);
+  		msg.data.capacity = PUB_MSG_CAPACITY;	
+  		snprintf(msg.data.data, msg.data.capacity, "Hello World %d", counter++);
+  		msg.data.size = strlen(msg.data.data);
+		RCSOFTCHECK(rcl_publish(&publisher, &msg, NULL));					
+	}	
+}
+
+void appMain(void * arg)
+{
+	rcl_allocator_t allocator = rcl_get_default_allocator();
+	rclc_support_t support;
+
+	// create init_options
+	RCCHECK(rclc_support_init(&support, 0, NULL, &allocator));
+
+	// create node
+	rcl_node_t node;
+	RCCHECK(rclc_node_init_default(&node, "freertos_string_publisher", "", &support));
+
+	// create publisher
+	RCCHECK(rclc_publisher_init_default(
+		&publisher,
+		&node,
+		ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, String),
+		"freertos_string_publisher"));
+
+	// create timer,
+	rcl_timer_t timer;
+	const unsigned int timer_timeout = 1000;
+	RCCHECK(rclc_timer_init_default(
+		&timer,
+		&support,
+		RCL_MS_TO_NS(timer_timeout),
+		timer_callback));
+
+	// create executor
+	rclc_executor_t executor;
+	RCCHECK(rclc_executor_init(&executor, &support.context, 1, &allocator));
+	RCCHECK(rclc_executor_add_timer(&executor, &timer));
+
+	while(1){
+		rclc_executor_spin_some(&executor, RCL_MS_TO_NS(100));		
+		usleep(100000);
+	}
+
+	// free resources
+	RCCHECK(rcl_publisher_fini(&publisher, &node))
+	RCCHECK(rcl_node_fini(&node))
+
+  	vTaskDelete(NULL);
+}
